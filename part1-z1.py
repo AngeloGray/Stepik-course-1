3f50e2b7a7ee236fa7c296f6966f3cad3919ec9b
AngeloGray <hasatik2001@gmail.com>
2022-09-04 16:19:22 +0300

    finished z1 in part 1

--- part1-z1.py
+++ part1-z1.py
@@ -1,5 +1,11 @@
 # В данной задаче требуется вычислить сумму двух входных целых чисел, лежащих в отрезке от нуля до десяти.
-
 import sys
-a, b = sys.stdin.read().split()
+# способ 1
+a, b = sys.stdin.readline().split()
 print(int(a) + int(b))
+# способ 2
+a = input()
+b = input()
+print(int(a) + int(b))
+# способ 3
+print(sum(map(int, input().split())))
